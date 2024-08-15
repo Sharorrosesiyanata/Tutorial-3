c347b7d4c5f6c1db226c6e25f1379462c7491213
Sharon-Rose Siyanata <sharonrosesiyanata7@gmail.com>
2024-08-15 18:13:21 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+"use strict";
+
+/********How to Convert HTML Form Field Values to JSON Object using JavaScript******/
+function convertToJson() {
+    let form = document.getElementById("dataForm");
+    let formData = {};
+    for (let i = 0; i < form.elements.length; i++) {
+        let element = form.elements[i];
+        if (element.type !== "submit") {
+            formData[element.name] = element.value;
+        }
+    }
+    let jsonData = JSON.stringify(formData);
+    let jsonOutput = document.getElementById("jsonOutput");
+    jsonOutput.innerHTML = "<pre>" + jsonData + "</pre>";
+}
+
+
+
+
+
+
+
